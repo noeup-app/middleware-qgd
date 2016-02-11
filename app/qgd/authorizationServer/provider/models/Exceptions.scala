38df8cbe4422755717490ce5e417c81bd7c1ee81
Guillaume de Ponsay <gdeponsay@cowebo.com>
2016-02-11 10:41:59 +0100

    [OAUTH2 PROVIDER] clean remove unused or deprecated files

--- app/qgd/authorizationServer/provider/models/Exceptions.scala
+++
@@ -1,4 +0,0 @@
-package com.yetu.oauth2provider.oauth2.models
-
-case class ImplicitFlowSyntaxException(msg: String) extends Exception
-case class ImplicitFlowException(msg: String) extends Exception
