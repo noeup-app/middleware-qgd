a86ec2733cd5c01acd9d234c11b0b6838480f2fa
Guillaume de Ponsay <gdeponsay@cowebo.com>
2016-02-26 10:17:04 +0100

    [OAUTH2 - AUTHORIZE] send auth code after concentment

--- app/qgd/middleware/authorizationServer/controllers/Authorize.scala
+++ app/qgd/middleware/authorizationServer/controllers/Authorize.scala
@@ -5,13 +5,13 @@ import javax.inject.Inject
 
 import com.mohiva.play.silhouette.api.{Environment, Silhouette}
 import com.mohiva.play.silhouette.impl.authenticators.CookieAuthenticator
+import play.api.db.DB
 import play.api.i18n.MessagesApi
 //import models.oauth2.Client
 //import play.api.db.slick.DBAction
 //import play.api.data._
 //import play.api.data.Forms._
-//import play.api.Play.current
-//import play.api.db.slick.DB
+import play.api.Play.current
 //import oauth2.OAuthDataHandler
 //import qgd.middleware.authorizationServer.models
 import qgd.middleware.authorizationServer.forms.AuthorizeForm
@@ -32,6 +32,8 @@ class Authorize @Inject()(val messagesApi: MessagesApi,
     "invalid_scope" -> "Requested scope is not allowed",
     "server_error" -> "Server encountered an error",
     "temporarily_unavailable" -> "Service is temporary unavailable")
+
+  // TODO comments
 
   def authorize = SecuredAction { implicit request =>
     // read URL parameters
@@ -59,41 +61,41 @@ class Authorize @Inject()(val messagesApi: MessagesApi,
     }
   }
 
-//  def send_auth = withUser { user =>
-//    implicit request =>
-//      val boundForm = AppAuthInfoForm.bindFromRequest
-//      boundForm.fold(
-//        formWithErrors => {
-//          log.debug(formWithErrors.toString)
-//          Ok(views.html.apps.authorize(user, formWithErrors))
-//        },
-//        aaInfo => {
-//          aaInfo.accepted match {
-//            case "Y" =>
-//              val expiresIn = Int.MaxValue
-//              val acOpt =
-//                DB.withSession { implicit session =>
-//                  models.oauth2.AuthCodes.generateAuthCodeForClient(
-//                    aaInfo.clientId, aaInfo.redirectUri, aaInfo.scope,
-//                    user.id.get, expiresIn)
-//                }
-//              acOpt match {
-//                case Some(ac) =>
-//                  val authCode = ac.authorizationCode
-//                  val state = aaInfo.state
-//                  Redirect(s"${aaInfo.redirectUri}?code=${authCode}&state=${state}")
-//                case None =>
-//                  val errorCode = "server_error"
-//                  Redirect(s"${aaInfo.redirectUri}?error=${errorCode}")
-//              }
-//
-//            case "N" =>
-//              val errorCode = "access_denied"
-//              Redirect(s"${aaInfo.redirectUri}?error=${errorCode}")
-//            case _ =>
-//              val errorCode = "invalid_request"
-//              Redirect(s"${aaInfo.redirectUri}?error=${errorCode}")
-//          }
-//        })
-//  }
+  // TODO comments
+  def send_auth = SecuredAction { implicit request =>
+      val boundForm = AuthorizeForm.form.bindFromRequest
+      boundForm.fold(
+        formWithErrors => {
+          log.debug(formWithErrors.toString)
+          Ok(views.html.apps.authorize(request.identity, formWithErrors))
+        },
+        aaInfo => {
+          aaInfo.accepted match {
+            case "Y" =>
+              val expiresIn = Int.MaxValue
+              val acOpt =
+                DB.withTransaction { implicit session =>
+                  models.AuthCode.generateAuthCodeForClient(
+                    aaInfo.clientId, aaInfo.redirectUri, aaInfo.scope,
+                    request.identity.id, expiresIn)
+                }
+              acOpt match {
+                case Some(ac) =>
+                  val authCode = ac.authorizationCode
+                  val state = aaInfo.state
+                  Redirect(s"${aaInfo.redirectUri}?code=${authCode}&state=${state}")
+                case None =>
+                  val errorCode = "server_error"
+                  Redirect(s"${aaInfo.redirectUri}?error=${errorCode}")
+              }
+
+            case "N" =>
+              val errorCode = "access_denied"
+              Redirect(s"${aaInfo.redirectUri}?error=${errorCode}")
+            case _ =>
+              val errorCode = "invalid_request"
+              Redirect(s"${aaInfo.redirectUri}?error=${errorCode}")
+          }
+        })
+  }
 }
