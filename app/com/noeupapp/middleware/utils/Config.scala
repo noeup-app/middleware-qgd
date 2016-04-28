6a3b57dbba4b009e39267e7bffb224050cb8bf5b
Damien RAYMOND <contact@damien-raymond.fr>
2016-04-28 12:44:24 +0200

    [0.1.0] Set default token expiration to 1 hour

--- app/com/noeupapp/middleware/utils/Config.scala
+++ app/com/noeupapp/middleware/utils/Config.scala
@@ -60,7 +60,7 @@ object Config {
     lazy val jsonWebTokenPrivateKeyFilename = Play.configuration.getString("security.jsonWebToken.privateKeyFilename").get
     lazy val jsonWebTokenPublicKeyFilename = Play.configuration.getString("security.jsonWebToken.publicKeyFilename").get
 
-    lazy val accessTokenExpirationInSeconds: Int = Play.configuration.getInt("security.expireTimes.accessTokenInSeconds").getOrElse(50000 * 60 * 1000)
+    lazy val accessTokenExpirationInSeconds: Long = Play.configuration.getLong("security.expireTimes.accessTokenInSeconds").getOrElse(60 * 60 * 1000)
 
     lazy val signatureDateExpirationInMilliseconds = Play.configuration.getLong("security.expireTimes.signatureInSeconds").get * MILLIS_PER_SECOND
 
