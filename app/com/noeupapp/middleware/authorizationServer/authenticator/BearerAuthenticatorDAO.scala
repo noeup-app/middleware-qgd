13e3e7bd1131685eeec1c5d7d3316b6717fd76fc
Raph-GA <raphael.gelin-alessi@laposte.net>
2016-06-13 16:29:15 +0200

    [TempRaph]Clean code

--- app/com/noeupapp/middleware/authorizationServer/authenticator/BearerAuthenticatorDAO.scala
+++ app/com/noeupapp/middleware/authorizationServer/authenticator/BearerAuthenticatorDAO.scala
@@ -24,10 +24,11 @@ class BearerAuthenticatorDAO(authAccessTokenService: OAuthAccessTokenService,
       case \/-(accessToken) => {
         Logger.debug(s"BearerAuthenticatorDAO.find($id) -> authAccessTokenService.find -> $accessToken")
         Logger.debug(s"BearerAuthenticatorDAO.find($id) -> userService.findById(${accessToken.userId}) ...")
-        userService.findById(accessToken.userId).map({
+        userService.findById(accessToken.userId).map{
           case \/-(Some(user)) => Some(user)
           case _ => None
-        }).map(x=>x.map({user => Logger.debug(s"BearerAuthenticatorDAO.find($id) -> userService.findById(${accessToken.userId}) -> $user")
+        }.map(_.map{user =>
+          Logger.debug(s"BearerAuthenticatorDAO.find($id) -> userService.findById(${accessToken.userId}) -> $user")
           val a = BearerTokenAuthenticator(id,
             api.LoginInfo("credentials", user.email.getOrElse("")),
             new DateTime(),
@@ -35,30 +36,15 @@ class BearerAuthenticatorDAO(authAccessTokenService: OAuthAccessTokenService,
             None
           )
           Logger.info(s"BearerAuthenticatorDAO.find($id) -> $a")
-          a}))}
+          a
+        })
+      }
 
       case -\/(e) =>
         Logger.debug(s"BearerAuthenticatorDAO.find($id) -> authAccessTokenService.find -> nothing found - $e")
         Future.successful(None)
     }
   }
-
-  /*
-          userService.findById(accessToken.userId).map({
-          case \/-(Some(user)) =>
-            Logger.debug(s"BearerAuthenticatorDAO.find($id) -> userService.findById(${accessToken.userId}) -> $user")
-            val a = BearerTokenAuthenticator(id,
-              api.LoginInfo("credentials", user.email.getOrElse("")),
-              new DateTime(),
-              new DateTime(accessToken.createdAt).plus(accessToken.expiresIn.getOrElse(0l)),
-              None
-            )
-            Logger.info(s"BearerAuthenticatorDAO.find($id) -> $a")
-            a
-
-          case _ => None
-        })}
-   */
 
   override def update(authenticator: BearerTokenAuthenticator): Future[BearerTokenAuthenticator] = {
     Logger.warn(s"AuthenticatorDAO.update($authenticator)")
