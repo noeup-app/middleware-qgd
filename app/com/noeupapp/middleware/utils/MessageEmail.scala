092683b0cfa21a0f4036ebde85c4a4b77b7da3cf
Raph-GA <raphael.gelin-alessi@laposte.net>
2016-06-15 10:44:19 +0200

    [TempRaph]Add messageEmail to utils with associated html template

---
+++ app/com/noeupapp/middleware/utils/MessageEmail.scala
@@ -0,0 +1,46 @@
+package com.noeupapp.middleware.utils
+
+import play.api.Play.current
+
+import scala.concurrent.duration._
+import play.api.libs.concurrent.Execution.Implicits._
+import play.api.Logger
+import java.io.File
+
+import scala.util.{Failure, Success, Try}
+import play.api.Play.current
+import play.api.i18n.Messages.Implicits._
+import views.html._
+import play.api.libs.mailer._
+import com.google.inject.Inject
+import com.noeupapp.middleware.errorHandle.FailError
+import com.noeupapp.middleware.errorHandle.FailError._
+import org.apache.commons.mail.EmailAttachment
+import com.noeupapp.middleware.views.html.emailTemplate
+
+import scala.concurrent.Future
+import scalaz._
+
+class MessageEmail @Inject()(mailerClient: MailerClient){
+
+
+  def sendEmail(senderName: Option[String], senderEmail: String, receiverName: String, receiverEmail: String,
+                 subject: String, text: String, appName: String, bcc: List[String] = Nil): Future[Expect[String]] = {
+
+    Try {
+      //IDEA shows an error but still compiles
+      val htmlTemplate = emailTemplate(subject, text, receiverName, senderName, appName)
+      val email = Email(
+        subject,
+        from = senderName.map(name => s"$name <$senderEmail>").getOrElse(senderEmail),
+        to = Seq(s"$receiverName <$receiverEmail>"),
+        bcc = bcc,
+        bodyHtml = Some(s"$htmlTemplate")
+      )
+      mailerClient.send(email)
+    } match {
+      case Failure(e) => Future.successful(-\/(FailError("Error while sending Email", e)))
+      case Success(res) => Future.successful(\/-(res))
+    }
+  }
+}
