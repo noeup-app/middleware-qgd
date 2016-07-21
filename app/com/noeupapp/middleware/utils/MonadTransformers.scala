dbe95015ec4036041af9c1931b087ed3cc527d74
Damien RAYMOND <dam.ray49@gmail.com>
2016-07-21 17:48:27 +0200

    [0.1.0] Add listMonadTransformation and convertListEitherToListTuple to MonadTransformers

--- app/com/noeupapp/middleware/utils/MonadTransformers.scala
+++ app/com/noeupapp/middleware/utils/MonadTransformers.scala
@@ -1,10 +1,13 @@
 package com.noeupapp.middleware.utils
 
 import com.noeupapp.middleware.errorHandle.FailError._
+
+import scala.concurrent.ExecutionContext.Implicits.global
 import play.api.Logger
 
 import scala.annotation.tailrec
-import scalaz.{-\/, \/-}
+import scala.concurrent.Future
+import scalaz.{-\/, \/, \/-}
 
 object MonadTransformers {
   @tailrec
@@ -18,5 +21,32 @@ object MonadTransformers {
       }
     case Nil                =>
       \/-(buffer)
+  }
+
+
+  def listMonadTransformation[T, U](list: List[T], function: T => Future[Expect[U]], identity: U): Future[Expect[U]] = list match {
+    case x :: xs  =>
+      function(x) flatMap {
+        case \/-(_) => listMonadTransformation(xs, function, identity)
+        case -\/(e) => Future {
+          -\/(e)
+        }
+      }
+    case Nil      => Future {
+      \/-(identity)
+    }
+  }
+
+
+  def convertListEitherToListTuple[A, E](l: List[E\/A]): (List[E], List[A]) = {
+    val r: (List[E\/A], List[E\/A]) =
+      l.partition(_.isLeft)
+    val left: List[E] = r._1
+      .map(_.toEither)
+      .foldLeft(List.empty[E]) ((acc, e) => acc ++ List(e.left.get))
+    val right: List[A] = r._2
+      .map(_.toEither)
+      .foldLeft(List.empty[A])((acc, e) => acc ++ List(e.right.get))
+    (left, right)
   }
 }
