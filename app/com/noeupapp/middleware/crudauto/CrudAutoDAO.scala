9654e8ac207dfc1f5695f47baab385f4cd2dbbb9
Raph-GA <raphael.gelin-alessi@laposte.net>
2016-08-09 14:45:27 +0200

    [0.1.0_54]FindById working with java reflection

--- app/com/noeupapp/middleware/crudauto/CrudAutoDAO.scala
+++ app/com/noeupapp/middleware/crudauto/CrudAutoDAO.scala
@@ -14,17 +14,21 @@ import scala.language.postfixOps
 
 class CrudAutoDAO extends GlobalReadsWrites {
 
-  def findById[T](classe: T, entityId: UUID, tableName: String)(implicit connection: Connection) = {
-    Logger.debug(classe.asInstanceOf[Class[T]].getName)
-    val parser = classe.asInstanceOf[Class[T]].getDeclaredField("parse")
-    parser.setAccessible(true)
-    Logger.debug(parser.toString)
+  def findById[T](model: T, entityId: UUID, tableName: String)(implicit connection: Connection) = {
+    val classe = model.asInstanceOf[Class[T]]
+    val const = classe.getDeclaredConstructors()(0)
+    const.setAccessible(true)
+    val obj = const.newInstance()
+    val parse = classe.getDeclaredField("parse")
+    parse.setAccessible(true)
+    val parser =  parse.get(classe.cast(obj)).asInstanceOf[anorm.RowParser[T]]
+
     SQL(
       s"""SELECT *
          FROM ${tableName}
          WHERE id = {id}::UUID;"""
       ).on(
         'id -> entityId
-      ).as(Entity.parse.asInstanceOf[anorm.RowParser[T]] *).headOption
+      ).as(parser *).headOption
   }
 }
