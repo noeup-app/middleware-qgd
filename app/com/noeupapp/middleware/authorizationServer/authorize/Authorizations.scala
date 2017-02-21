739d91d8dc32c17dd43371d898fcd2e1ac6369cf
quentin <quentin@noeupapp.com>
2017-02-21 10:07:24 +0100

    [#76/CleanMiddleStarter] Signup template + signing flash data

--- app/com/noeupapp/middleware/authorizationServer/authorize/Authorizations.scala
+++ app/com/noeupapp/middleware/authorizationServer/authorize/Authorizations.scala
@@ -172,11 +172,15 @@ class Authorizations @Inject()(val messagesApi: MessagesApi,
         }.recover {
           case e: ProviderException =>
             Logger.warn("Logins.authenticate failed : " + authenticate + " -> " + e.getMessage)
-            Redirect(com.noeupapp.middleware.authorizationServer.authorize.routes.Authorizations.login(data.client_id.toString, data.redirect_uri, data.state, data.scope))
+            /*Redirect(com.noeupapp.middleware.authorizationServer.authorize.routes.Authorizations.login(data.client_id.toString, data.redirect_uri, data.state, data.scope))
+              .flashing("error" -> Messages("invalid.credentials"))*/
+            Redirect(com.noeupapp.middleware.authorizationClient.login.routes.Logins.loginAction())
               .flashing("error" -> Messages("invalid.credentials"))
           case e: Exception => {
             Logger.error("An exception ocurred", e)
-            Redirect(com.noeupapp.middleware.authorizationServer.authorize.routes.Authorizations.login(data.client_id.toString, data.redirect_uri, data.state, data.scope))
+            /*Redirect(com.noeupapp.middleware.authorizationServer.authorize.routes.Authorizations.login(data.client_id.toString, data.redirect_uri, data.state, data.scope))
+              .flashing("error" -> Messages("internal.server.error"))*/
+            Redirect(com.noeupapp.middleware.authorizationClient.login.routes.Logins.loginAction())
               .flashing("error" -> Messages("internal.server.error"))
           }
         }
