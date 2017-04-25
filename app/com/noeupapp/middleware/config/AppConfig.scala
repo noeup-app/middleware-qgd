d74a98730ce4945561739c1d583b6c5de73fbedc
Guillaume de Ponsay <guillaume@noeupapp.com>
2017-04-25 14:30:15 +0200

    quick fix merge

--- app/com/noeupapp/middleware/config/AppConfig.scala
+++ app/com/noeupapp/middleware/config/AppConfig.scala
@@ -1,7 +1,3 @@
 package com.noeupapp.middleware.config
 
-<<<<<<< HEAD
 case class AppConfig(appUrl: String, uploadPath: String)
-=======
-case class AppConfig(appUrl: String, uploadPath: String)
->>>>>>> c2c33b67b45698bbdb6328f850f7ceda7ced5e6e
