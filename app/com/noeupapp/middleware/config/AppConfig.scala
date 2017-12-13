bff24d692e5fe4c038c6f8c517dc49b192b760b0
Guillaume de Ponsay <guillaume@noeupapp.com>
2017-12-13 17:43:30 +0100

    Modify conf to set env vars for multiple installs

--- app/com/noeupapp/middleware/config/AppConfig.scala
+++ app/com/noeupapp/middleware/config/AppConfig.scala
@@ -1,3 +1,13 @@
 package com.noeupapp.middleware.config
 
-case class AppConfig(appUrl: String, uploadPath: String, frontUrl: String)
+
+//case class AppConfig(appUrl: String, uploadPath: String, frontUrl: String)
+
+case class AppConfig(appUrl: String,
+                     uploadPath: String,
+                     frontUrl: String,
+                     url:String,
+                     logo:String,
+                     name: String,
+                     namehtml: String
+                    )
